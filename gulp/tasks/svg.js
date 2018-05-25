432831d3fabcac94372a5a91281a2021fb4143f8
biletskiy6 <victorbiletskiy82@gmail.com>
2018-05-25 20:20:23 +0300

    update task svg

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -1,30 +1,20 @@
 'use strict';
 
 module.exports = function() {
-	$.gulp.task('svg', function() {
+		$.gulp.task('svg', function() {
 		return $.gulp.src('./src/static/img/svg/*.svg')
-		.pipe($.gp.svgmin({
-			js2svg: {
-				pretty: true
-			}
+		.pipe($.gp.svgstore({
+			inlineSvg: true
 		}))
-		.pipe($.gp.cheerio({
-			run: function($){
-				$('[fill]').removeAttr('fill');
-				$('[stroke]').removeAttr('stroke');
-				$('[style]').removeAttr('style');
-			},
-			parseOptions: {xmlMode: true}
-		}))
-		.pipe($.gp.replace('&gt;','>'))
-		.pipe($.gp.svgSprite({
-			mode: {
-				symbol: {
-					sprite: 'sprite.svg'
-				}
-			}
-		}))
+		 .pipe($.gp.cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+            },
+            parserOptions: { xmlMode: true }
+        }))
+		.pipe($.gp.rename("sprite.svg"))
 		.pipe($.gulp.dest('./build/static/img/svg/'))
 	});
+	
 	
 }
